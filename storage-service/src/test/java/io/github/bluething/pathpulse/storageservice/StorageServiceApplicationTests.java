9d9d6cb8072fb6af0a2c0aaf331c748838bfaa08
Habib Ali Machpud <habib.machpud@gmail.com>
2026-03-09 00:03:23 +0700

    Init modules

---
+++ storage-service/src/test/java/io/github/bluething/pathpulse/storageservice/StorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.pathpulse.storageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
